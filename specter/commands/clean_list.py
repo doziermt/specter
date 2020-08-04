b29ac1399b488d433ea16691f936281ce0183e65
Mike <doziermt@gmail.com>
2020-08-04 12:22:22 -0500

    Add more comments and error handling

--- specter/commands/clean_list.py
+++ specter/commands/clean_list.py
@@ -36,7 +36,13 @@ class CleanList(Command):
         super().__init__()
 
     def execute(self):
+        # Create a temporary file in order to temporarily dump out all the contents to a file
+        # since we cannot capture stdout directly. This allows subprocess.run to dump stdout
+        # to the terminal; afterward, we can re-analyze the stdout output by opening up the
+        # tempfile, formatting its contents, and dumping the formatted output to the real
+        # "clean target list" file.
         _, temporary_file = tempfile.mkstemp(prefix='specter')
+
         command = [
             self.APPLICATION, "-sL", "-n", "-iL", self.target_list_file_path,
             "--excludefile", self.exclude_list_file_path, "-oN", temporary_file
@@ -50,7 +56,9 @@ class CleanList(Command):
         with open(self.xml_clean_target_list_file_path, 'w') as outfile:
             outfile.writelines(formatted_lines)
 
-    def _validate_target_file_path(self, path, settings_name):
+    def _validate_target_file_path(self, path, settings_alias):
+        # If the file doesn't exist, quickly create it.
+        # Also, if the file does already exist, make sure that it is not a directory.
         if not os.path.exists(path):
             try:
                 os.utime(path, None)
@@ -60,5 +68,9 @@ class CleanList(Command):
         else:
             if not os.path.isfile(path):
                 raise FileNotFoundError(
-                    'The "%s" must reference a file, not a directory' %
-                    settings_name)
+                    'Failed to locate file: The "%s" option in settings.toml must reference a file, not a directory'
+                    % settings_alias)
+            if not os.access(path, os.R_OK):
+                raise IOError(
+                    'Failed to read file: The "%s" option in settings.toml must be a readable file'
+                    % settings_alias)
