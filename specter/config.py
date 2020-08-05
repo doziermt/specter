3ad0dad6a4fb7bde1bdd590ba3166b8503f044eb
Mike <doziermt@gmail.com>
2020-08-05 11:35:26 -0500

    Add tox job for validating sample config contents

--- specter/config.py
+++ specter/config.py
@@ -1,6 +1,9 @@
 import os
+import sys
+import traceback
 
 from dynaconf import Dynaconf, Validator
+from dynaconf.validator import ValidationError
 
 
 def _build_output_directory_hierarchy():
@@ -8,11 +11,11 @@ def _build_output_directory_hierarchy():
         os.makedirs(dirname, exist_ok=True)
 
 
-def validate_settings():
+def validate_settings(config_file_name='settings.toml'):
     settings = Dynaconf(
         environments=False,
         load_dotenv=False,
-        settings_files=[os.path.join(os.getcwd(), 'settings.toml')],
+        settings_files=[os.path.join(os.getcwd(), config_file_name)],
         validators=[
             Validator('general', must_exist=True),
             Validator('general.sitename', must_exist=True, is_type_of=str),
@@ -51,4 +54,16 @@ def validate_settings():
     return settings
 
 
-settings = validate_settings()
+if __name__ == '__main__':
+    # Validate the sample config file if calling this via tox.
+    try:
+        sample_conf_rel_path = 'samples/settings.sample.toml'
+        validate_settings(sample_conf_rel_path)
+    except ValidationError as e:
+        tb = sys.exc_info()[2]
+        traceback.print_exception(e.__class__, e, tb, limit=2, file=sys.stdout)
+        error_message = 'Dynaconf validation failed for config file: %s' % sample_conf_rel_path
+        sys.exit(error_message)
+else:
+    # Otherwise imported from specter.py, load & validate the settings.
+    settings = validate_settings()
